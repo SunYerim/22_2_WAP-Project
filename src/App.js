3f357d15042cf1043148293aac156817df6f6860
Sun Yerim <syl1602@naver.com>
2022-11-16 14:23:57 +0900

    #8 Login Register Page useState

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
